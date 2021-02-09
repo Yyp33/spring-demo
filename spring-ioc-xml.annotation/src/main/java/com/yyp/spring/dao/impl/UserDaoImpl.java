fff608755c7a6346fd718f7e9716b32c2c9fb63f
yyp <1092957495@qq.com>
2021-02-09 11:01:47 +0800

    增加spring注解模块

---
+++ spring-ioc-xml.annotation/src/main/java/com/yyp/spring/dao/impl/UserDaoImpl.java
@@ -0,0 +1,9 @@
+package com.yyp.spring.dao.impl;
+
+import org.springframework.stereotype.Repository;
+
+import com.yyp.spring.dao.UserDao;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+}
---
+++ spring-ioc-xml.annotation/src/main/java/com/yyp/spring/service/impl/UserServiceImpl.java
@@ -0,0 +1,9 @@
+package com.yyp.spring.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.yyp.spring.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
